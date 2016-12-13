f952e319ab2eb44f2cf70de25a2fb6c8ab18cc6f
Srikanth Gurram <2gurram@gmail.com>
2016-12-13 08:30:00 +0530

    added basic listners

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-var specPassCount = 0;
-var specFailCount = 0;
-var testResults = [];
-var testSuites = [];
-
-module.exports = {
-    printMsg: function() {
-	console.log("This is a message from the demo package");
-    },
-
-    jasmineStarted: function (suiteInfo) {
+function jasmineHtmlReporter() {
+    var self = this;
+    var specPassCount = 0;
+    var specFailCount = 0;
+    var testResults = [];
+    var testSuites = [];
+	
+    self.jasmineStarted = function (suiteInfo) {
+        console.log('#############################################################');
         console.log('Running suite with ' + suiteInfo.totalSpecsDefined + 'exampls');
-        console.log('suiteInfo ' + suiteInfo);
-    },
+    };
 
-    suiteStarted: function (result) {
+    self.suiteStarted = function (result) {
         console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
-    },
+    };
 
-    specStarted: function (result) {
+    self.specStarted = function (result) {
         console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
-    },
+    };
 
-    specDone: function (result) {
+    self.specDone = function (result) {
         testResults.push(result);
         console.log('---------------------------------------------------------------------');
         console.log('-------------------------SPEC COMPLETED------------------------------');
@@ -39,9 +36,9 @@ module.exports = {
             console.log(result.failedExpectations[i].stack);
         }
         console.log(result.passedExpectations.length);
-    },
+    };
 
-    suiteDone: function (result) {
+    self.suiteDone = function (result) {
         testSuites.push(result);
         console.log('Suite: ' + result.description + ' was ' + result.status);
         for (var i = 0; i < result.failedExpectations.length; i++) {
@@ -52,14 +49,17 @@ module.exports = {
         console.log('------------------------SUITE COMPLETED------------------------------');
         console.log(result);
         console.log('---------------------------------------------------------------------');
-    },
+    };
 
-    jasmineDone: function () {
+    self.jasmineDone = function () {
         console.log('Finished suite');
-        console.log('#######################################################################')
-        console.log(testResults);
-        console.log('---------------------------------------------------------------------');
+	console.log('-----------------------------TEST RESULTS----------------------------------------');
+  	console.log(testResults);
+
+        console.log('-----------------------------TEST SUITES----------------------------------------');
         console.log(testSuites);
-        console.log('#######################################################################')
-    }
+        console.log('#######################################################################');
+    };
 };
+
+module.exports = jasmineHtmlReporter;
